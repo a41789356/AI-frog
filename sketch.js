0ca8de5a0e4d23754ea828ca63828efd1c801d19
a41789356 <112880658+a41789356@users.noreply.github.com>
2022-09-06 15:52:46 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://a41789356.github.io/AI-frog/model.json';
+let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
 
 
 function preload() {
@@ -50,5 +50,4 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
-  ThunkableWebviewerExtension.postMessage(label);
 }
